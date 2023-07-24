9d34e08075affeb16bee5519fd6fd68fa8749003
vinic <vinic@DESKTOP-NQFDHH5>
2023-07-24 18:54:42 -0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ImageProcessing;
+
+const string IMAGE_DIR_PATH = "C:\\Users\\vinic\\OneDrive\\Imagens\\Saved Pictures\\";
+
+string[] files = Directory.GetFiles(IMAGE_DIR_PATH);
+
+for (int i = 0; i < files.Length; i++)
+    Console.WriteLine($"[{i}] {files[i].Replace(IMAGE_DIR_PATH, "")}");
+
+Console.Write("\nFile #: ");
+string fileChosen = files[int.Parse(Console.ReadLine())];
+
+ImageManager imageManager = new(fileChosen);
---
+++ ImageProcessing/ImageManager.cs
@@ -0,0 +1,17 @@
+﻿namespace ImageProcessing;
+
+public class ImageManager
+{
+    public ImageManager(string imagePath)
+    {
+		try
+		{
+			Image<Rgba32> image = Image.Load<Rgba32>(imagePath);
+			Console.WriteLine(">> DEBUG: image loaded");
+		}
+		catch (UnknownImageFormatException _)
+		{
+            Console.WriteLine(">> DEBUG: image NOT loaded");
+		}
+    }
+}
