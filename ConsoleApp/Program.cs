083eb7a4456574419e041a34d2f8420e8cb722fa
BuenoVini <51279927+BuenoVini@users.noreply.github.com>
2023-07-25 02:07:17 -0300

    First attempt to apply Mean Blur to an image

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -20,7 +20,7 @@ Console.Clear();
 
 WiderpaperManager.LoadImage(fileChosen);
 
-Console.WriteLine("[0] Mirror Algorithm");
+Console.WriteLine("[0] Apply Mirror\n[1] Apply Mean Blur");
 
 Console.Write("\nSelect algorithm: ");
 int algorithmChosen = int.Parse(Console.ReadLine());
@@ -29,9 +29,8 @@ Console.WriteLine("\nProcessing...");
 
 switch (algorithmChosen)
 {
-    case 0: 
-        WiderpaperManager.ApplyMirror();
-        break;
+    case 0: WiderpaperManager.ApplyMirror(); break;
+    case 1: WiderpaperManager.ApplyMeanBlur(); break;
 }
 
 WiderpaperManager.SaveImage(IMAGE_DIR_PATH + "output.jpg");
--- ImageProcessing/WiderpaperManager.cs
+++ ImageProcessing/WiderpaperManager.cs
@@ -1,32 +1,43 @@
-﻿using Widerpaper.ResizeMethods;
-
-namespace Widerpaper;
+﻿namespace Widerpaper;
 
 public static class WiderpaperManager
 {
 	private static Image<Rgba32>? _imgInput;
 	private static Image<Rgba32>? _imgOutput;
 
+    #region Private Methods
+    private static void ValidateImageInput()
+    {
+        if (_imgInput is null)
+            throw new NullReferenceException("No image was loaded to memory.");
+    }
+
+    private static void ValidateImageOutput()
+    {
+        if (_imgOutput is null)
+            throw new NullReferenceException("No image was resized yet to be saved.");
+    }
+    #endregion
+
+    #region Public Methods
     public static void LoadImage(string imgPath) => _imgInput = Image.Load<Rgba32>(imgPath);
 	public static void SaveImage(string imgPath)
 	{
-		if (_imgOutput is null)
-			throw new NullReferenceException("No image was resized yet to be saved.");
+		ValidateImageOutput();
 
-		_imgOutput.Save(imgPath);
+		_imgOutput!.Save(imgPath);
 	}
 
 	public static void ApplyMirror()
 	{
-		if (_imgInput is null)
-			throw new NullReferenceException("No image was loaded to memory.");
+        ValidateImageInput();
 
         /* ALGORITHM FROM MY PREVIOUS PROJECT (https://github.com/BuenoVini/Widerpaper/) */
 
-        _imgOutput = new(_imgInput.Height * 21 / 9, _imgInput.Height);
+        _imgOutput = new(_imgInput!.Height * 21 / 9, _imgInput.Height);
 
         /* an odd image has one extra column of pixels on the right side. so it must be treated separately */
-        bool isOddImage = (_imgOutput.Width - _imgInput.Width) % 2 != 0;
+        bool isOddImage = (_imgOutput.Width - _imgInput!.Width) % 2 != 0;
 
         /* the quantity of pixels to be mirrored copied to the side of the input image */
         int qtyPixelsToCopy = (_imgOutput.Width - _imgInput.Width) / 2;
@@ -59,4 +70,61 @@ public static class WiderpaperManager
             }
         }
     }
+
+    public static void ApplyMeanBlur(int kernelSize = 3)
+    {
+        ValidateImageInput();
+
+        _imgOutput = new(_imgInput!.Width, _imgInput.Height);
+
+        /* creating the convolution matrix */
+        int[,] kernel = new int[kernelSize, kernelSize];
+        for (int i = 0; i < kernelSize; i++)
+            for (int j = 0; j < kernelSize; j++)
+                kernel[i, j] = 1;
+
+        var temp = new Rgba32[kernelSize, kernelSize];
+
+        /* applying the kernel to the entire image */
+        for (int y = 0; y < _imgInput!.Height; y++)
+        {
+            for (int x = 0; x < _imgInput.Width; x++)
+            {
+                byte sumR = 0, sumG = 0, sumB = 0;
+
+                for (int i = 0; i < kernelSize; i++)
+                {
+                    for (int j = 0; j < kernelSize; j++)
+                    {
+                        int relativeX = x + j - (kernelSize - 1) / 2;
+                        int relativeY = y + i - (kernelSize - 1) / 2;
+
+                        if (relativeX < 0 || relativeY < 0 || relativeX >= _imgInput.Width || relativeY >= _imgInput.Height )
+                        {
+                            temp[i, j].R = 0;
+                            temp[i, j].G = 0;
+                            temp[i, j].B = 0;
+                        }
+                        else
+                        {
+                            temp[i, j].R = (byte)(_imgInput[relativeX, relativeY].R * kernel[i, j]);
+                            temp[i, j].G = (byte)(_imgInput[relativeX, relativeY].G * kernel[i, j]);
+                            temp[i, j].B = (byte)(_imgInput[relativeX, relativeY].B * kernel[i, j]);
+                        }
+
+                        sumR += temp[i, j].R;
+                        sumG += temp[i, j].G;
+                        sumB += temp[i, j].B;
+                    }
+                }
+
+                sumR = (byte)(sumR / kernelSize * kernelSize);
+                sumG = (byte)(sumG / kernelSize * kernelSize);
+                sumB = (byte)(sumB / kernelSize * kernelSize);
+
+                _imgOutput[x, y] = new Rgba32(sumR, sumG, sumB);
+            }
+        }
+    }
+    #endregion
 }
