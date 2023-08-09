409efe1ac469f4355e23249a8754c0818d3f4f87
BuenoVini <51279927+BuenoVini@users.noreply.github.com>
2023-08-09 13:16:40 -0300

    Disabled the maximize button on Windows

--- BlazorApp/MauiProgram.cs
+++ BlazorApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace BlazorApp
 {
@@ -16,8 +17,31 @@ namespace BlazorApp
 
             builder.Services.AddMauiBlazorWebView();
 
+        #if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                // Make sure to add "using Microsoft.Maui.LifecycleEvents;" in the top of the file 
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.IsMaximizable = false;
+                                break;
+                        }
+                    });
+                });
+            });
+        #endif
+
         #if DEBUG
-		    builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
 		    builder.Logging.AddDebug();
         #endif
 
