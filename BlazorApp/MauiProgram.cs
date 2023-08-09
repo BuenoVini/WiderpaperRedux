6e88bdfafe7166c125bb145dcdf2e5acc3a3b4d5
BuenoVini <51279927+BuenoVini@users.noreply.github.com>
2023-08-09 12:38:37 -0300

    Fixed build issue when project was cleaned

--- BlazorApp/MauiProgram.cs
+++ BlazorApp/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using BlazorApp.Data;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace BlazorApp
 {
@@ -21,8 +20,6 @@ namespace BlazorApp
 		    builder.Services.AddBlazorWebViewDeveloperTools();
 		    builder.Logging.AddDebug();
         #endif
-
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             return builder.Build();
         }
